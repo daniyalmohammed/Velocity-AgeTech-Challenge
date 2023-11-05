46c77e377de95e2780bd246b6ef42782a8985d22
Dhruv <dhruvgop@gmail.com>
2023-11-04 22:26:21 -0400

    added cards for equipment detail

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './Login';
 import EquipmentList from './EquipmentList';
 import EquipmentDetail from './EquipmentDetail';
 import './App.css';
+// import AddCardForm from './AddCardForm';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Login setUser={setUser} />} />
           <Route path="/equipment-list" element={<EquipmentList user={user} />} />
           <Route path="/equipment/:id" element={<EquipmentDetail />} />
+          {/* <Route path="/add-card/:id" component={<AddCardForm />} /> */}
         </Routes>
       </div>
     </Router>
--- src/EquipmentDetail.js
+++ src/EquipmentDetail.js
@@ -1,25 +1,90 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const details = {
-  '1': ['Hospital A', 'Hospital B'],
-  '2': ['Hospital C', 'Hospital D'],
-  '3': ['Hospital E', 'Hospital F'],
-};
+const equipmentData = [
+  {
+    id: '1',
+    name: 'Equipment A',
+    hospitals: [
+      {
+        name: 'Hospital A',
+        cost: '$1000',
+        stock: 10,
+        estimatedTime: '2 days',
+        brand: 'ACME'
+      },
+      {
+        name: 'Hospital B',
+        cost: '$1200',
+        stock: 5,
+        estimatedTime: '3 days',
+        brand: 'ACME'
+      },
+    ],
+  },
+  {
+    id: '2',
+    name: 'Equipment B',
+    hospitals: [
+      {
+        name: 'Hospital C',
+        cost: '$1500',
+        stock: 5,
+        estimatedTime: '3 days',
+        brand: 'ACME'
+      },
+      {
+        name: 'Hospital D',
+        cost: '$1300',
+        stock: 8,
+        estimatedTime: '4 days',
+        brand: 'ACME'
+      },
+    ],
+  },
+  {
+    id: '3',
+    name: 'Equipment C',
+    hospitals: [
+      {
+        name: 'Hospital E',
+        cost: '$800',
+        stock: 15,
+        estimatedTime: '1 day',
+        brand: 'ACME'
+      },
+      {
+        name: 'Hospital F',
+        cost: '$900',
+        stock: 20,
+        estimatedTime: '2 days',
+        brand: 'ACME'
+      },
+    ],
+  },
+];
 
 function EquipmentDetail() {
   const { id } = useParams();
-  const hospitals = details[id] || [];
+  const equipment = equipmentData.find(item => item.id === id);
 
   return (
-    <div>
+    <div className="equipment-list">
       <h1>Equipment Detail</h1>
-      <h2>Available at:</h2>
-      <ul>
-        {hospitals.map((hospital, index) => (
-          <li key={index}>{hospital}</li>
+      <div className="card-container">
+        {equipment.hospitals.map((hospital, index) => (
+          <div className="card" key={index}>
+            <img src={require("./images/hospital-image.jpg")} alt={hospital.name} className="card-image" />
+            <div className="card-content">
+              <h3 className="card-title">{hospital.name}</h3>
+              <p>Brand: {hospital.brand}</p>
+              <p>Cost: {hospital.cost}</p>
+              <p>Number in Stock: {hospital.stock}</p>
+              <p>Estimated Time: {hospital.estimatedTime}</p>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
