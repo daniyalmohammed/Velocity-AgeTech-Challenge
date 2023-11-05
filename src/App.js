7070617955abe055f2205db7f809979680d692bb
daniyalmohammed <d25moham@uwaterloo.ca>
2023-11-05 12:54:00 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import EquipmentList from './EquipmentList';
 import EquipmentDetail from './EquipmentDetail';
 import './App.css';
-// import AddCardForm from './AddCardForm';
+// Import the Navbar CSS
+import './Navbar.css';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -12,11 +13,25 @@ function App() {
   return (
     <Router>
       <div className="App">
+        {/* Navigation Bar */}
+        <nav className="navbar">
+          <Link to="/" className="navbar-brand">MedMobilize</Link>
+          <div className="navbar-nav">
+            <Link to="/" className="nav-item">Home</Link>
+            <Link to="/about" className="nav-item">About</Link>
+            <Link to="/equipment-list" className="nav-item">Rent</Link>
+            <Link to="/sell" className="nav-item">Sell</Link>
+            <Link to="/contact" className="nav-item">Contact</Link>
+            <Link to="/login" className="nav-item">Sign up / Log in</Link>
+          </div>
+        </nav>
+
+        {/* Main Content Area */}
         <Routes>
           <Route path="/" element={<Login setUser={setUser} />} />
           <Route path="/equipment-list" element={<EquipmentList user={user} />} />
           <Route path="/equipment/:id" element={<EquipmentDetail />} />
-          {/* <Route path="/add-card/:id" component={<AddCardForm />} /> */}
+          {/* Add more routes here as needed */}
         </Routes>
       </div>
     </Router>
